5da477efb7e8e410605c5ab0fb18f2954d6459f3
Carl Vincent Dictaan <carldictaan@gmail.com>
2026-09-20 06:41:23 +0800

    practice of if and else and while loop

--- src/JOptionPane/training.java
+++ src/JOptionPane/training.java
@@ -1,56 +1,47 @@
-import java.util.Scanner;
-public class training {
+package JOptionPane; // 1. Fixes the red error
 
+import java.util.Scanner;
+
+public class training {
 
     public static void main(String[] args) {
 
-
         Scanner input = new Scanner(System.in);
 
-        String ask = "";
-        String confirmationId = "";
-        String finalAct = "";
+        String ask; // 2. Removed redundant ""
+        String confirmationId;
 
-        while (true){
+        while (true) {
             System.out.print("ARE YOU A STUDENT?: ");
             ask = input.nextLine().trim();
 
-            if(ask.equalsIgnoreCase("Y")){
-                System.out.println("PROCESSS.........");
+            if (ask.equalsIgnoreCase("Y")) {
+                System.out.println("PROCESSING.........");
 
-                while(true){
-
-                    System.out.print("DO YOU HAVE AN STUDENT ID?|| Y OR N: ");
-
+                while (true) {
+                    System.out.print("DO YOU HAVE A STUDENT ID? || Y OR N: ");
                     confirmationId = input.nextLine().trim();
 
-                    if(confirmationId.equalsIgnoreCase("Y")){
-
-                        System.out.print("WELCOME!!!");
+                    if (confirmationId.equalsIgnoreCase("Y")) {
+                        System.out.println("WELCOME!!!");
                         break;
-
-                    }
-
-                    else if(confirmationId.equalsIgnoreCase("n")){
-
+                    } else if (confirmationId.equalsIgnoreCase("N")) {
                         System.out.println("YOU ARE NOT ALLOWED HERE");
                         break;
-
-                    }
-                    else{
+                    } else {
                         System.out.println("INVALID ANSWER!!");
-                        continue;
+                        // Removed redundant continue
                     }
                 }
-                    break;
-            } else if(ask.equalsIgnoreCase("N")){
-                System.out.print("GET OUT NIGGER");
                 break;
-            } else{
-                System.out.print("ENTER THE RIGHT VALUE || Y or N ?");
-                continue;
-            }
 
+            } else if (ask.equalsIgnoreCase("N")) {
+                System.out.println("YOU ARE NOT ALLOWED HERE");
+                break;
+            } else {
+                System.out.println("ENTER THE RIGHT VALUE || Y or N?");
+                // Removed redundant continue
+            }
         }
         input.close();
     }
